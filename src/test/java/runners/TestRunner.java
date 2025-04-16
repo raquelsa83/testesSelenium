0399db7847db6a96dfe5660412e4cec1aa04dcc7
Globalhitss <Global@RJONB005016.globalhitss.com.br>
2025-04-16 15:49:51 -0300

    implementacao JUnit5

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,18 +1,16 @@
 package runners;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "steps",
-        plugin = {"pretty", 
-        	   "html:target/cucumber-reports",
-               //"html:target/cucumber-reports/cucumber.html", // Relatório HTML
-               //"json:target/cucumber-reports/cucumber.json"  // Relatório JSON
-            }
-)
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "steps")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, 
+                       value = "pretty, html:target/cucumber-reports")
 public class TestRunner {
 }
